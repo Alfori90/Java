15bb3d5212c9951462ea00bf76008e3a004c86bb
Huria <alforihuria938@gmail.com>
2025-06-26 14:18:22 +0400

    My CV

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello Code Creators");
+        System.out.println("Iam Huria Alfori");
+        System.out.println("Iam from Muscat.I recently graduated with a bacholar degree in Electronics and Telecommunication Engineering from Higher college of Technology.I have had many training courses where I developed  my skills.Iam an ambitious person with the ability to expand my knowledge and awareness in telecommunication filed.I approach any task with a pro-active attitude and like to be creative where necessary and where possible.  ");
+        System.out.println("The last training I joined was in Ooredoo Network Operations Center. It was in operation management center department.I have  excellent interpersonal skills and knowledge of computing applications like Ms office ppt and Excel and word.Also Iam familiar with the basics in CISCO ");
+    }
+}
