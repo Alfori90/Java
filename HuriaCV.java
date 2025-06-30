d3aaeea484a315556e545385a49d3bf545a26552
Huria <alforihuria938@gmail.com>
2025-06-30 11:23:51 +0400

    HuriaCV

---
+++ HuriaCV.java
@@ -0,0 +1,14 @@
+public class HuriaCV {
+
+    public static void main(String[] args){
+        System.out.println("Huri Abdullah Alfori");
+        System.out.println("Sultant Of Oman/Muscat/Busher");
+        System.out.println("Bachelor of science Degree in Telecommunication Engineering from higher college of Technology with GPA 3.36");
+        System.out.println("Final year project was about develop and implement of smart doorbell:it comprises of wireless indoor and outdoor unit");
+        System.out.println("The last training I was joined in Ooredoo Network Operation Center:it was in Operation managment center department that dealing with business customer from end to end");
+        System.out.println("Also I joined training program with HUAWEI ICT Academy:it was in latest technologys 4G and 5G");
+        System.out.println("Skills:Excellent knowledge of computing application and excellent command of both written and spoken English and Arabic ");
+        System.out.println("I have familiar with the basics in CISCO");
+   
+    }
+}
